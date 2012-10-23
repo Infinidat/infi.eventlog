a7877d27ff2111e07bb4b416d89611b97b66704b
Guy Rozendorn <guy@rzn.co.il>
2012-10-23 20:35:53 +0200

    TRIVIAL parse event xml

--- src/infi/eventlog/c_api/mock.py
+++ src/infi/eventlog/c_api/mock.py
@@ -1,6 +1,7 @@
 import os
 import ctypes
 import infi.cwrap
+import textwrap
 
 HANDLE = ctypes.c_void_p
 EVT_HANDLE = HANDLE
@@ -19,6 +20,17 @@ ERROR_NO_MORE_ITEMS = 259
 open_handles = dict()
 max_handle_id = 1
 available_channel_names = [u"System", u"Application"]
+
+SAMPLE_XML = u"""<Event xmlns='http://schemas.microsoft.com/win/2004/08/events/event'>
+<System><Provider Name='Service Control Manager' Guid='{555908d1-a6d7-4695-8e1e-2
+6931d2012f4}' EventSourceName='Service Control Manager'/><EventID Qualifiers='16
+384'>7036</EventID><Version>0</Version><Level>4</Level><Task>0</Task><Opcode>0</
+Opcode><Keywords>0x8080000000000000</Keywords><TimeCreated SystemTime='2010-11-2
+1T03:31:33.034185300Z'/><EventRecordID>1</EventRecordID><Correlation/><Execution
+ ProcessID='484' ThreadID='564'/><Channel>System</Channel><Computer>37L4247F26-2
+4</Computer><Security/></System><EventData><Data Name='param1'>User Profile Serv
+ice</Data><Data Name='param2'>stopped</Data><Binary>500072006F006600530076006300
+2F0031000000</Binary></EventData></Event>""".replace("\n", "")
 
 class WindowsException(Exception):
     def __init__(self, winerror):
@@ -112,4 +124,5 @@ def EvtCreateRenderContext(values_path_count, values_paths, flags):
 def EvtRender(context, fragment, flags, buffer_size, buffer, buffer_used_size, property_count):
     assert context in open_handles.keys() + [None]
     assert fragment.value in open_handles
+    buffer._obj.value = unicode(SAMPLE_XML)
     return 1
